e4e1437c1f6cc94ba4f7f7b9def70ad9adf405f9
nathanideal26 <nathanideal26@gmail.com>
2026-08-10 13:31:56 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,73 @@
-const startButton =
-    document.getElementById("startButton");
+const beginTourButton =
+    document.getElementById(
+        "beginTourButton"
+    );
 
-startButton.addEventListener(
+const sleepVideo =
+    document.getElementById(
+        "sleepVideo"
+    );
+
+
+beginTourButton.addEventListener(
     "click",
-    startTour
+    beginTour
 );
 
 
-function startTour() {
+function beginTour() {
 
-    console.log(
-        "NOVUS TOUR STARTED"
+    if (sleepVideo) {
+
+        sleepVideo.pause();
+
+    }
+
+
+    beginTourButton.innerHTML =
+        "NOVUS IS WAKING UP...";
+
+
+    beginTourButton.disabled =
+        true;
+
+
+    document.body.classList.add(
+        "waking-up"
     );
 
-    /*
-    Paparan 2 akan kita
-    sambungkan kemudian.
-    */
 
-    alert(
-        "NOVUS IS WAKING UP..."
-    );
+    setTimeout(() => {
+
+        /*
+        PAPARAN 2 AKAN KITA
+        SAMBUNG KEMUDIAN.
+
+        Nanti kita tukar kepada:
+
+        window.location.href =
+        "language.html";
+        */
+
+        alert(
+            "NOVUS IS READY"
+        );
+
+
+        beginTourButton.innerHTML =
+            "TOUCH HERE TO BEGIN TOUR";
+
+
+        beginTourButton.disabled =
+            false;
+
+
+        if (sleepVideo) {
+
+            sleepVideo.play();
+
+        }
+
+    }, 1500);
+
 }
