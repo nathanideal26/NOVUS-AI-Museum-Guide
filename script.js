17c552378ac9e8e9ea3b57112aea6d20c8639c9f
nathanideal26 <nathanideal26@gmail.com>
2026-08-10 13:17:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+const startButton =
+    document.getElementById("startButton");
 
+startButton.addEventListener(
+    "click",
+    startTour
+);
+
+
+function startTour() {
+
+    console.log(
+        "NOVUS TOUR STARTED"
+    );
+
+    /*
+    Paparan 2 akan kita
+    sambungkan kemudian.
+    */
+
+    alert(
+        "NOVUS IS WAKING UP..."
+    );
+}
